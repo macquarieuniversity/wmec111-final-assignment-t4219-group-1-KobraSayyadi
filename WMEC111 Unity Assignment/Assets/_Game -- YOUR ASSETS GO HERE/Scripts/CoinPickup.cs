e3b4fe10304c1dbf5f0873f8db46e3ed1b376857
Kobra Sayyadi <kobra.sayyadi@students.mq.edu.au>
2019-09-04 16:34:31 +1000

    added coin pickup

---
+++ WMEC111 Unity Assignment/Assets/_Game -- YOUR ASSETS GO HERE/Scripts/CoinPickup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPickup : MonoBehaviour {
+
+	public LayerMask playerLayer;
+	private AudioSource _audio;
+	private bool isGone = false;
+
+	public void Start() {
+		_audio = gameObject.GetComponent<AudioSource>();
+	}
+
+	public void OnTriggerEnter2D(Collider2D collider) {		
+		if (isGone) return;
+
+		// check if the colliding object is in the playerLayer
+		if (((1 << collider.gameObject.layer) & playerLayer.value) != 0) {
+			// play a sound if there is one
+			if (_audio != null) {
+				_audio.Play();
+			}
+
+			// hide the sprite and disable this script
+			SpriteRenderer _renderer = gameObject.GetComponent<SpriteRenderer>();
+			_renderer.enabled = false;
+			isGone = true;
+		}
+	}
+
+}
